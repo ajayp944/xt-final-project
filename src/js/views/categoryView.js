da668717df60fab0d793c6abd195a63b4eafc56e
Ajay Pratap Singh <ajayp944@gmail.com>
2019-01-07 14:31:11 +0530

    category order

--- src/js/views/categoryView.js
+++ src/js/views/categoryView.js
@@ -13,44 +13,24 @@ export const renderResults = (category) => {
 // Render Category in view port
 const renderCategory = (key, category) => {
     let markup;
+    markup = `<div class="category">
+        <div class="category__box ${(key + 2) % 2 ? 'category__box--order' : ''}">
+            <div class="category__box--title">
+                ${category.name}
+            </div>
+            <p class="category__box--description">
+                ${category.description}
+            </p>
+            <div>
+                <a class="btn btn--pink category__box--button" href="product.html#${category.id}">Explore fruit-and-veg</a>
+            </div>
+        </div>
+        <div class="category__box">
+            <img class="category__img" src="${category.imageUrl}" alt="${category.name}">
+        </div>
+    </div>
+    <div class="divider" aria-hidden="true">&nbsp;</div>`;
 
-    if ((key + 2) % 2) {
-        markup = `<div class="category">
-            <div class="category__box">
-                <div class="category__box--title">
-                    ${category.name}
-                </div>
-                <p class="category__box--description">
-                    ${category.description}
-                </p>
-                <div>
-                    <a class="btn btn--pink category__box--button" href="product.html#${category.id}">Explore fruit-and-veg</a>
-                </div>
-            </div>
-            <div class="category__box">
-                <img class="category__img" src="${category.imageUrl}" alt="${category.name}">
-            </div>
-        </div>
-        <div class="divider" aria-hidden="true">&nbsp;</div>`;
-    } else {
-        markup = `<div class="category">
-            <div class="category__box">
-                <img class="category__img" src="${category.imageUrl}" alt="${category.name}">
-            </div>
-            <div class="category__box">
-                <div class="category__box--title">
-                    ${category.name}
-                </div>
-                <p class="category__box--description">
-                    ${category.description}
-                </p>
-                <div>
-                    <a class="btn btn--pink category__box--button" href="product.html#${category.id}">Explore ${category.name}</a>
-                </div>
-            </div>
-        </div>
-        <div class="divider" aria-hidden="true">&nbsp;</div>`;
-    }
     elements.categoryContainer.insertAdjacentHTML('beforeend', markup);
 };
 
